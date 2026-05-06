13371523e21cc30bccbebc00f044b9de6888c550
Alejo Rodriguez <alejandro.vargasr@uqvirtual.edu.co>
2026-05-05 20:06:59 -0500

    iniciio

---
+++ gimnasio/lib/gimnasio.ex
@@ -0,0 +1,18 @@
+defmodule Gimnasio do
+  @moduledoc """
+  Documentation for `Gimnasio`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gimnasio.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ inventario/lib/inventario.ex
@@ -0,0 +1,18 @@
+defmodule Inventario do
+  @moduledoc """
+  Documentation for `Inventario`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Inventario.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
